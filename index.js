7c8ce7a13a6797efce4055f20e62dadd06286c00
hoai.tran <hoai.tran.insenco@gmail.com>
2022-04-20 16:41:06 +0700

    Revert "[FEATURE] Implement post api"
    This reverts commit b38e7f47e5f1cfc874e74b992d12a5715c1e6168.

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
-// const postRouter = require("./a");
-require("dotenv").config();
-
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const postRouter = require("./src/routers/post.js");
-const port = 9999;
 const app = express();
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+const port = 9999;
 
-mongoose
-  .connect(process.env.DB_CONNECTION, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Connected to the database");
-  });
-// mongoose.set("useFindAndModify", false);
-app.get("/", (req, res) => {
-  res.send("Hello word!");
+//middleware
+app.use(express.json());
+
+app.use("/", (req, res) => {
+  res.send("Hello word!")
 });
-
-app.use("/posts", postRouter);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
